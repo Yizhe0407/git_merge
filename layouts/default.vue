70c7a3c4c7148a91995fbe857cf736c2174c67f2
yizhe <liaoyizhe75@gmail.com>
2024-09-15 11:09:56 +0800

    Refactor layout and homepage components

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <Header />
+        <div class="flex-grow">
+            <slot />
+        </div>
+        <Footer />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
-    <div class="flex flex-col min-h-screen">
-        <Header />
-        <div class="flex-grow flex items-center justify-center">
-            <p class="text-3xl font-bold">Homepage</p>
-        </div>
-        <Footer />
+    <div class="flex h-full items-center justify-center">
+        <p class="text-3xl font-bold">Homepage</p>
     </div>
 </template>
